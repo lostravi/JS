e9488f1c45af9f9c04ec1bb5275eceb1ca486aa6
Abhi3004-web <abhijitranjan1@gmail.com>
2024-10-05 13:36:48 +0530

    update array.

--- Hello.js
+++ Hello.js
@@ -1,4 +1,4 @@
 console.log("Hello Data javascript. message edited.");
 console.log("Edited by ravi");
-const arr = [1,2,3,4,5,6];
-const arr1 = []
+const arr = [1, 2, 3, 4, 5, 6];
+const arr1 = [3, 4, 5, 6, 7, 8, 9]
